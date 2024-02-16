be938f7ad3c4f4b39c27be55b28eea56348eb1fd
Melanie <melanie.collins.p@gmail.com>
2024-02-16 15:25:17 -0300

    agregue smooth scroll a navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
-});
+// $(function () {
+//     $(document).scroll(function () {
+//         var $nav = $("#mainNavbar");
+//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//     });
+// });
